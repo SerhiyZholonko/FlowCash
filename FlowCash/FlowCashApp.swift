f679b68b21880ecf47bfa9204739369e3fbc929f
ZholonkoS <41541490+SerhiyZholonko@users.noreply.github.com>
2026-05-25 08:42:42 +0300

    first

---
+++ FlowCash/FlowCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowCashApp.swift
+//  FlowCash
+//
+//  Created by apple on 25.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlowCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
